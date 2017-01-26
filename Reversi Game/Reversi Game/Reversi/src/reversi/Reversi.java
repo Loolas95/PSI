7ad471e84533969f3811d746b33443f9bc8e48cc
Karol <loolas95@gmail.com>
2017-01-26 15:12:51 +0100

    AI

--- Reversi Game/Reversi Game/Reversi/src/reversi/Reversi.java
+++ Reversi Game/Reversi Game/Reversi/src/reversi/Reversi.java
@@ -114,7 +114,14 @@ public class Reversi {
         change = 0;
         int mincounter;
         int numberOfMoves[] = new int[1];
-   
+        //STRATEGY
+        //0-normal
+        //1-corners
+        //2-keeping the corners out
+        //3-stable disc
+        //4-min_max algorithm
+        int option=4;
+
 
 
         for (int i = 0; i < rows; ++i)
@@ -122,13 +129,60 @@ public class Reversi {
             for (int j = 0; j < columns; ++j)
             {
                 if(gameCells[i][j].getCh() == '.') {
+                    if (option == 4) {
+                        copyArray();
+                        mincounter=0;
+                        change=1;
+                        move2(i, j, change, 'O', 'X', numberOfMoves, tmpgameCells);
+                        mincounter+=numberOfMoves[0];
+
+                        if(numberOfMoves[0]>0) {
+                           ArrayList list=minmaxplayerplay();
+                            for(int count=0;count<list.size();count++){
+                                int counter1=mincounter;
+                                counter1+=list.indexOf(i);
+                                if(max<counter1){
+                                    max=counter1;
+                                    mX=i;
+                                    mY=j;
+                                }
+
+                            }
+                        }
+
+                    } else {
+
+
+
+
+
+
+
                         move(i, j, change, 'O', 'X', numberOfMoves, gameCells);
                         if (max <= numberOfMoves[0]) {
+                            if (max != 0 && option == 2 && nearcorner(i, j)) {
+                            } else {
                                 max = numberOfMoves[0];
                                 mX = i;
                                 mY = j;
                             }
-                      }
+                        }
+                        if (option == 1) {
+                            if ((i == 0 && j == 0 || i == rows && j == 0 || i == 0 && j == columns || i == rows && j == columns) && option == 1 && numberOfMoves[0] > 0) {
+                                max = 100;
+                                mX = i;
+                                mY = j;
+                            }
+                        }
+                        if (option == 3) {
+                            if (i + j < 8 && option == 3 && numberOfMoves[0] > 0) {
+                                max = 100;
+                                mX = i;
+                                mY = j;
+                            }
+                        }
+                    }
+                }
             }
         }	
         computerMove = max;
@@ -144,7 +198,110 @@ public class Reversi {
         }
         return 0;
     }
-public int play(int xCor,int yCor) // play function for user
+    public int minmaxcompplay() // minmaxplay function for computer
+    {
+        int change,max = 0,mX = 0,mY = 0;
+        change = 0;
+        int numberOfMoves[] = new int[1];
+
+
+        for (int i = 0; i < rows; ++i)
+        {
+            for (int j = 0; j < columns; ++j)
+            {
+                if(tmpgameCells[i][j].getCh() == '.') {
+                        move2(i, j, change, 'O', 'X', numberOfMoves, tmpgameCells);
+                        if (max <= numberOfMoves[0]) {
+                                max = numberOfMoves[0];
+                                mX = i;
+                                mY = j;
+                            }
+                }
+            }
+        }
+        computerMove = max;
+        if (computerMove == 0)
+        {
+            computerMove = -1;
+            return computerMove;
+        }
+        if(computerMove != 0)
+        {
+            change = 1;
+            move2(mX,mY,change,'O','X',numberOfMoves,tmpgameCells);
+        }
+        return computerMove;
+    }
+    public ArrayList<Integer> minmaxplayerplay() // minmaxplay function for user
+    {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        int change,max = 0;
+        int numberOfMoves[] = new int[1];
+        change = 0;
+        copyArray1();
+        for (int i = 0; i < rows; ++i)
+        {
+            for (int j = 0; j < columns; ++j)
+            {
+                if(tmpgameCells[i][j].getCh() == '.')
+                {
+                   copytmp1Totmp();
+                    change=1;
+                    move2(i,j,change,'X','O',numberOfMoves,tmpgameCells);
+                    if(numberOfMoves[0]>0) {
+                        max = numberOfMoves[0];
+                        max+=minmaxcompplay();
+                        list.add(max);
+                    }
+                }
+            }
+        }
+      return list;
+    }
+    private void copyArray() {
+        for(int i = 0; i< rows; i++){
+            for (int j = 0; j < columns; j++){
+                int x = gameCells[i][j].getX();
+                char c = gameCells[i][j].getCh();
+                int y = gameCells[i][j].getY();
+                tmpgameCells[i][j].setCell(x,c,y);
+
+            }
+        }
+    }
+    private void copyArray1() {
+        for(int i = 0; i< rows; i++){
+            for (int j = 0; j < columns; j++){
+                int x = tmpgameCells[i][j].getX();
+                char c = tmpgameCells[i][j].getCh();
+                int y = tmpgameCells[i][j].getY();
+                tmp1gameCells[i][j].setCell(x,c,y);
+
+            }
+        }
+    }
+    private void copytmp1Totmp() {
+        for(int i = 0; i< rows; i++){
+            for (int j = 0; j < columns; j++){
+                int x = tmp1gameCells[i][j].getX();
+                char c = tmp1gameCells[i][j].getCh();
+                int y = tmp1gameCells[i][j].getY();
+                tmpgameCells[i][j].setCell(x,c,y);
+
+            }
+        }
+    }
+
+
+    public boolean nearcorner(int i, int j){
+        if ((i==0 && j==1)||(i==1 && j==1)||(i==1 && j==0)) return true;
+        if ((i==rows-2 && j==0)||(i==rows-2 && j==1)||(i==rows-1 && j==1)) return true;
+        if ((i== columns -2 && j==0)||(i== columns -2 && j==1)||(i== columns -1 && j==0)) return true;
+        if ((i==rows-1 && j== columns -1)||(i==rows-2 && j== columns -1)||(i==rows-1 && j== columns -2)) return true;
+        return false;
+
+    }
+    public int play(int xCor,int yCor) // play function for user
     {  
         int status;
         int change,max = 0; 
@@ -565,3 +722,306 @@ public int play(int xCor,int yCor) // play function for user
 	else
             return -1;			
     }
+    int move2(int xCor, int yCor,int change,char char1,char char2,int []numberOfMoves, Cell [][] tmpgameCells)
+    {
+        int cont,emptyfield=0,color=0;
+        int status = -1,corX,corY,temp;
+        int iy;
+        int ix,y,x;
+
+        x = xCor; y = yCor;
+        numberOfMoves[0] = 0;
+        if((x+1 < rows) && ( tmpgameCells[x+1][y].getCh() == char2)) //east
+        {
+
+            cont = x;
+            while((cont < rows)  && (emptyfield != -1) && (color != 2))
+            {
+                cont++;
+                if(cont < rows){
+                    if(tmpgameCells[cont][y].getCh() == char2)
+                        color = 1;
+                    else if(tmpgameCells[cont][y].getCh() == char1)
+                        color = 2;
+                    else
+                        emptyfield = -1;
+                }
+            }
+            if (color == 2)
+            {
+                temp = cont - x - 1;
+                numberOfMoves[0] += temp;
+            }
+            if(color == 2 && change == 1)
+            {
+                for (int i = x; i < cont; ++i)
+                {
+                    iy = tmpgameCells[i][y].getX();
+                    ix = tmpgameCells[i][y].getY();
+                    tmpgameCells[i][y].setCell(iy,char1,ix);
+                }
+                status = 0;
+            }
+            color=0;
+        }
+        if((x-1 >= 0) && (tmpgameCells[x-1][y].getCh() == char2)) //west
+        {
+
+            cont = x;
+            while((cont >= 0) && (emptyfield != -1) && (color != 2))
+            {
+                cont--;
+                if(cont >= 0){
+                    if(tmpgameCells[cont][y].getCh() == char2)
+                        color = 1;
+                    else if(tmpgameCells[cont][y].getCh() == char1)
+                        color = 2;
+                    else
+                        emptyfield = -1;
+                }
+            }
+            if (color == 2)
+            {
+                temp = x - cont - 1;
+                numberOfMoves[0] += temp;
+            }
+            if(color == 2 && change == 1)
+            {
+                for (int i = cont; i <= x; ++i)
+                {
+                    iy = tmpgameCells[i][y].getX();
+                    ix = tmpgameCells[i][y].getY();
+                    tmpgameCells[i][y].setCell(iy,char1,ix);
+                }
+                status = 0;
+            }
+            color=0;emptyfield=0;
+        }
+        if((y+1 < columns) && (tmpgameCells[x][y+1].getCh() == char2)) //south
+        {
+
+            cont = y;
+            while((cont < columns) && (emptyfield != -1) && (color != 2))
+            {
+                cont++;
+                if(cont < columns){
+                    if(tmpgameCells[x][cont].getCh() == char2)
+                        color = 1;
+                    else if(tmpgameCells[x][cont].getCh() == char1)
+                        color = 2;
+                    else
+                        emptyfield = -1;
+                }
+            }
+            if (color == 2)
+            {
+                temp = cont - y - 1;
+                numberOfMoves[0] += temp;
+            }
+            if(color == 2 && change == 1)
+            {
+                for (int i = y; i < cont; ++i)
+                {
+                    iy = tmpgameCells[x][i].getX();
+                    ix = tmpgameCells[x][i].getY();
+                    tmpgameCells[x][i].setCell(iy,char1,ix);
+                }
+                status = 0;
+            }
+            color=0;emptyfield=0;
+        }
+        if((y-1 >= 0) && (tmpgameCells[x][y-1].getCh() == char2)) //north
+        {
+
+            cont = y;
+            while((cont >= 0) && (emptyfield != -1) && (color != 2))
+            {
+                cont--;
+                if(cont >= 0){
+                    if(tmpgameCells[x][cont].getCh() == char2)
+                        color = 1;
+                    else if(tmpgameCells[x][cont].getCh() == char1)
+                        color = 2;
+                    else
+                        emptyfield = -1;
+                }
+            }
+            if (color == 2)
+            {
+                temp = y - cont - 1;
+                numberOfMoves[0] += temp;
+            }
+            if(color == 2 && change == 1)
+            {
+                for (int i = cont; i <= y; ++i)
+                {
+                    iy = tmpgameCells[x][i].getX();
+                    ix = tmpgameCells[x][i].getY();
+                    tmpgameCells[x][i].setCell(iy,char1,ix);
+                }
+                status = 0;
+            }
+            color=0;emptyfield=0;
+        }
+        if((x-1 >= 0) && (y+1 < columns) && (tmpgameCells[x-1][y+1].getCh() == char2)) //SE
+        {
+
+            corY = y;
+            corX = x;
+            while((corX >= 0) && (corY < columns) && (emptyfield != -1) && (color != 2))
+            {
+                corX--;
+                corY++;
+                if((corX >= 0) && (corY < columns)){
+                    if(tmpgameCells[corX][corY].getCh() == char2)
+                        color = 1;
+                    else if(tmpgameCells[corX][corY].getCh() == char1)
+                        color = 2;
+                    else
+                        emptyfield = -1;
+                }
+            }
+            if (color == 2)
+            {
+                temp = x - corX - 1;
+                numberOfMoves[0] += temp;
+            }
+            if(color == 2 && change == 1)
+            {
+                while((corX <= x) && (y < corY))
+                {
+                    corX++;
+                    corY--;
+
+                    if((corX <= x) && (y <= corY)){
+                        iy = tmpgameCells[corX][corY].getX();
+                        ix = tmpgameCells[corX][corY].getY();
+                        tmpgameCells[corX][corY].setCell(iy,char1,ix);
+                    }
+                }
+                status = 0;
+            }
+            color=0;emptyfield=0;
+        }
+        if((x-1 >= 0) && (y-1 >= 0) && (tmpgameCells[x-1][y-1].getCh() == char2)) //NW
+        {
+            corY = y;
+            corX = x;
+            while((corX >= 0) && (corY >= 0) && (emptyfield != -1) && (color != 2))
+            {
+                corX--;
+                corY--;
+                if((corX >= 0) && (corY >= 0)){
+                    if(tmpgameCells[corX][corY].getCh() == char2)
+                        color = 1;
+                    else if(tmpgameCells[corX][corY].getCh() == char1)
+                        color = 2;
+                    else
+                        emptyfield = -1;
+                }
+            }
+            if (color == 2)
+            {
+                temp = x - corX - 1;
+                numberOfMoves[0] += temp;
+            }
+            if(color == 2 && change == 1)
+            {
+                while((corX <= x) && (corY <= y))
+                {
+                    corX++;
+                    corY++;
+                    if((corX <= x) && (corY <= y)){
+                        iy = tmpgameCells[corX][corY].getX();
+                        ix = tmpgameCells[corX][corY].getY();
+                        tmpgameCells[corX][corY].setCell(iy,char1,ix);
+                    }
+                }
+                status = 0;
+            }
+            color=0;emptyfield=0;
+        }
+        if((x+1 < rows) && (y+1 < columns) && (tmpgameCells[x+1][y+1].getCh() == char2)) //SW
+        {
+
+            corY = y;
+            corX = x;
+            while((corX < rows) && (corY < columns) && (emptyfield != -1) && (color != 2))
+            {
+                corX++;
+                corY++;
+                if((corX < rows) && (corY < columns)){
+                    if(tmpgameCells[corX][corY].getCh() == char2)
+                        color = 1;
+                    else if(tmpgameCells[corX][corY].getCh() == char1)
+                        color = 2;
+                    else
+                        emptyfield = -1;
+                }
+            }
+            if (color == 2)
+            {
+                temp = corX - x - 1;
+                numberOfMoves[0] += temp;
+            }
+            if(color == 2 && change == 1)
+            {
+                while((corX >= x) && (corY >= y))
+                {
+                    corX--;
+                    corY--;
+                    if((corX >= x) && (corY >= y)){
+                        iy = tmpgameCells[corX][corY].getX();
+                        ix = tmpgameCells[corX][corY].getY();
+                        tmpgameCells[corX][corY].setCell(iy,char1,ix);
+                    }
+                }
+                status = 0;
+            }
+            color=0;emptyfield=0;
+        }
+        if((x+1 < rows) && (y-1 >= 0) && (tmpgameCells[x+1][y-1].getCh() == char2)) //NE
+        {
+
+            corY = y;
+            corX = x;
+            while((corX < rows) && (corY >= 0) && (emptyfield != -1) && (color != 2))
+            {
+                corX++;
+                corY--;
+                if((corX < rows) && (corY >= 0)){
+                    if(tmpgameCells[corX][corY].getCh() == char2)
+                        color = 1;
+                    else if(tmpgameCells[corX][corY].getCh() == char1)
+                        color = 2;
+                    else
+                        emptyfield = -1;
+                }
+            }
+            if (color == 2)
+            {
+                temp = corX - x - 1;
+                numberOfMoves[0] += temp;
+            }
+            if(color == 2 && change == 1)
+            {
+                while((corX >= x) && (corY <= y))
+                {
+                    corX--;
+                    corY++;
+                    if((corX >= x) && (corY <= y)){
+                        iy = tmpgameCells[corX][corY].getX();
+                        ix = tmpgameCells[corX][corY].getY();
+                        tmpgameCells[corX][corY].setCell(iy,char1,ix);
+                    }
+                }
+                status = 0;
+            }
+
+        }
+        if(status == 0)
+            return 0;
+        else
+            return -1;
+    }
+}
